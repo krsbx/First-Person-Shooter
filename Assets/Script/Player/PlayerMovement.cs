3e5abb0088ca4b7bea0722c1c69bfd1737b0c9ab
KRSBX <firdaus.sati50@gmail.com>
2020-12-26 17:53:16 +0700

    Adding New Movement
    Mouse Look Added
    Mouse Cursor Locked To The Screen
    Player Running Function Added
    Player Crouching Function Added
    Custom Key for Crouch, Run, Jump in Inspector Added

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -6,18 +6,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //Player Attributes
-    [Range(0, 200)] public float MouseSensitivity = 100f, CurrentSpeed = 20f;
+    [Range(0, 200)] public float MouseSensitivity = 100f, DefaultSpeed = 20f;
+    [Range(0, 200)] public float CurrentSpeed = 20f, RunSpeed = 100f, CrouchSpeed = 20f;
     //Jump Attributes
     [Range(0.2f, 20)] public float DistCheck = 0.2f, GravityForces = 10f, JumpForces = 10f;
+    //Special Keys
+    public KeyCode Crouch = KeyCode.LeftControl, Run = KeyCode.LeftShift, Jump = KeyCode.Space;
     [HideInInspector] public Rigidbody rb; [HideInInspector] public CapsuleCollider cc;
     //Private Variables
     private Vector3 checkPos; //For Ground Checking
+    private float rotationY; //For Mouse Look
     private void Start() {
         //Get All Component
         rb = GetComponent<Rigidbody>();
         cc = GetComponent<CapsuleCollider>();
         //Freeze Rigidbody Rotation
         rb.constraints = RigidbodyConstraints.FreezeRotation;
+        //Lock Cursor
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     bool IsGrounded(){
@@ -34,8 +40,25 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Update() {
-        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        //Jumping
+        if(Input.GetKeyDown(Jump) && IsGrounded())
             rb.velocity = Vector3.up*JumpForces;
+        
+        //Crouching
+        if(Input.GetKeyDown(Crouch)){
+            cc.height /=2;
+            CurrentSpeed = CrouchSpeed;
+        }else if(Input.GetKeyUp(Crouch)){
+            cc.height *= 2;
+            CurrentSpeed = DefaultSpeed;
+        }
+
+        //Running
+        if(Input.GetKeyDown(Run)){
+            CurrentSpeed = RunSpeed;
+        }else if(Input.GetKeyUp(Run)){
+            CurrentSpeed = DefaultSpeed;
+        }
 
         //Set checkPos to the center of the player
         //  Centered X dan Z
@@ -65,5 +88,15 @@ public class PlayerMovement : MonoBehaviour
         //Ground Forces
         if(rb.velocity.y < -2f)
             rb.velocity = Vector3.down*GravityForces;
+
+        //Player Camera
+        rotationY -= MouseY;
+        rotationY = Mathf.Clamp(rotationY, -90f, 90f); //Limit the player view
+        
+        //Up and Down
+        Camera.main.transform.localRotation = Quaternion.Euler(rotationY, 0, 0);
+
+        //Left and Right
+        rb.rotation *= Quaternion.Euler(0, MouseX, 0);
     }
 }
