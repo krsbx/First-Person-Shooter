eeb3a0479227f80dff78bdef768f3a1c6c285adf
KRSBX <firdaus.sati50@gmail.com>
2020-12-26 17:59:06 +0700

    Initial Commit Head Bob

---
+++ Assets/Script/Player/PlayerHeadBob.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerHeadBob : MonoBehaviour
+{
+    PlayerMovement movement; //Used for player data
+    private void Start() {
+        //Get All Component
+        movement = GetComponent<PlayerMovement>();
+    }
+
+    //Get All Player Movement Key From PlayerMovement
+    public KeyCode Run {get{return movement.Run;}}
+    public KeyCode Crouch {get{return movement.Crouch;}}
+    //Get All Player Component
+    public Rigidbody rb {get{return movement.rb;}}
+    public Vector3 localPos{
+        get{ return Camera.main.transform.localPosition; }
+        set{ Camera.main.transform.localPosition = value; }
+    }
+
+}
