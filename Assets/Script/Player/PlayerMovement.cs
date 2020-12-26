89754a74026e05b4b66b03dbf96164e612904e81
KRSBX <firdaus.sati50@gmail.com>
2020-12-26 17:37:40 +0700

    Add Jump Functions

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -7,13 +7,40 @@ public class PlayerMovement : MonoBehaviour
 {
     //Player Attributes
     [Range(0, 200)] public float MouseSensitivity = 100f, CurrentSpeed = 20f;
+    //Jump Attributes
+    [Range(0.2f, 20)] public float DistCheck = 0.2f, GravityForces = 10f, JumpForces = 10f;
     [HideInInspector] public Rigidbody rb; [HideInInspector] public CapsuleCollider cc;
+    //Private Variables
+    private Vector3 checkPos; //For Ground Checking
     private void Start() {
         //Get All Component
         rb = GetComponent<Rigidbody>();
         cc = GetComponent<CapsuleCollider>();
         //Freeze Rigidbody Rotation
         rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    bool IsGrounded(){
+        RaycastHit hit;
+        //Create a Sphere Cast
+        //  Origin in checkPos (under the players)
+        //  With size radius/4 with directions under the player
+        //  Distance to check = DistCheck
+        //  Cast to all layer (-1)
+        if(Physics.SphereCast(checkPos, cc.radius/4, Vector3.down, out hit, DistCheck, -1, QueryTriggerInteraction.Ignore))
+            return true;
+        else
+            return false;
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+            rb.velocity = Vector3.up*JumpForces;
+
+        //Set checkPos to the center of the player
+        //  Centered X dan Z
+        //  The Y Axis => Centered Y -  Half Bounds in Y Axis + DistCheck
+        checkPos = new Vector3(cc.bounds.center.x, cc.bounds.center.y-cc.bounds.extents.y+DistCheck, cc.bounds.center.z);
     }
 
     private void LateUpdate() {
@@ -34,5 +61,9 @@ public class PlayerMovement : MonoBehaviour
         
         //Set Player Movement
         rb.velocity = new Vector3(velocity.x*CurrentSpeed, rb.velocity.y, velocity.y*CurrentSpeed);
+
+        //Ground Forces
+        if(rb.velocity.y < -2f)
+            rb.velocity = Vector3.down*GravityForces;
     }
 }
